4f00e31304351d96fafa9ae459b0b84c43d3e486
Siddeshwar <siddeshwar.work@gmail.com>
2022-06-29 07:08:40 +0530

    added css animations for overview page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,26 @@ module.exports = {
         'purple': {
           700: '#512DA8'
         }
-      }
+      },
+      animation: {
+        'live-now': 'liveNow 2s ease infinite',
+        'list-from-top': 'listFromTop 300ms ease-out'
+      },
+      keyframes: {
+        liveNow: {
+          '0%': { opacity: .7 },
+          '50%': { opacity: 1 },
+          '100%': { opacity: .7 },
+        },
+        listFromTop: {
+          '0%': {
+            transform: 'translateY(-20px)'
+          },
+          '100%': {
+            transform: 'translateY(0)'
+          }
+        }
+      },
     },
   },
   plugins: [],
