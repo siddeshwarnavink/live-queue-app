14c424c0a69c82b8ca4d73bd6f738a7edf14cf5a
Siddeshwar <siddeshwar.work@gmail.com>
2022-06-30 08:55:01 +0530

    disabling next button when the up coming list is loading

--- src/app/ui/button/button.component.ts
+++ src/app/ui/button/button.component.ts
@@ -6,6 +6,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() flat = false;
+  @Input() isDisable = false;
   @Output() click = new EventEmitter();
 
   constructor() { }
